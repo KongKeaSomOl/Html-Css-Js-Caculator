b7675148d3cf5fe171cf7cace11a9a830a95cf79
KongKeaSomOl <144871689+KongKeaSomOl@users.noreply.github.com>
2024-08-20 12:09:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// Caculator Program
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function caculator() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
